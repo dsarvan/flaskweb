d5fa4b6474eba0475a3c03accbb75cda203f358c
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-08-06 19:32:10 +0530

    blueprint stores project main pages

---
+++ flaskweb/pages.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from flask import Blueprint, render_template
+
+bp = Blueprint("pages", __name__)
+
+@bp.route("/")
+def home():
+	return render_template("pages/home.html")
+
+@bp.route("/about")
+def about():
+	return render_template("pages/about.html")
